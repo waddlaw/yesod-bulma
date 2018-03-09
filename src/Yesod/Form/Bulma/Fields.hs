69d8660fad650e7f86f64a1d0cd3922137beda1d
waddlaw <ingronze@gmail.com>
2018-03-09 18:40:51 +0900

    Refactor

--- src/Yesod/Form/Bulma/Fields.hs
+++ src/Yesod/Form/Bulma/Fields.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE QuasiQuotes           #-}
 module Yesod.Form.Bulma.Fields where
 
-import           Control.Monad            (unless)
+import           Control.Monad            (unless, forM_)
 import           Data.Maybe               (listToMaybe)
 import           Data.Text                (Text)
 import           Data.Text.Encoding       (decodeUtf8With, encodeUtf8)
@@ -24,13 +24,13 @@ import           Yesod.Form.Types         (Enctype (..), Field (..))
 -- | Creates a input with @type="text"@.
 textField :: Monad m => RenderMessage (HandlerSite m) FormMessage => Field m Text
 textField = Field
-    { fieldParse = parseHelper $ Right
-    , fieldView = \theId name attrs val isReq ->
-        [whamlet| $newline never
-          <input id="#{theId}" .input name="#{name}" *{attrs} type="text" :isReq:required value="#{either id id val}">
-        |]
-    , fieldEnctype = UrlEncoded
-    }
+  { fieldParse = parseHelper Right
+  , fieldView = \theId name attrs val isReq ->
+      [whamlet| $newline never
+        <input id="#{theId}" .input name="#{name}" *{attrs} type="text" :isReq:required value="#{either id id val}">
+      |]
+  , fieldEnctype = UrlEncoded
+  }
 
 -- | Creates an input with @type="email"@. Yesod will validate the email's correctness according to RFC5322 and canonicalize it by removing comments and whitespace (see "Text.Email.Validate").
 emailField :: Monad m => RenderMessage (HandlerSite m) FormMessage => Field m Text
@@ -50,13 +50,13 @@ emailField = Field
 -- | Creates a @\<textarea>@ tag whose returned value is wrapped in a 'Textarea'; see 'Textarea' for details.
 textareaField :: Monad m => RenderMessage (HandlerSite m) FormMessage => Field m Textarea
 textareaField = Field
-    { fieldParse = parseHelper $ Right . Textarea
-    , fieldView = \theId name attrs val isReq ->
-        [whamlet| $newline never
-          <textarea id="#{theId}" .textarea name="#{name}" :isReq:required="" *{attrs}>#{either id unTextarea val}
-        |]
-    , fieldEnctype = UrlEncoded
-    }
+  { fieldParse = parseHelper $ Right . Textarea
+  , fieldView = \theId name attrs val isReq ->
+      [whamlet| $newline never
+        <textarea id="#{theId}" .textarea name="#{name}" :isReq:required="" *{attrs}>#{either id unTextarea val}
+      |]
+  , fieldEnctype = UrlEncoded
+  }
 
 -- | Creates a @\<select>@ tag for selecting one option. Example usage:
 --
@@ -73,20 +73,20 @@ selectField :: (Eq a, RenderMessage site FormMessage)
             => HandlerT site IO (OptionList a)
             -> Field (HandlerT site IO) a
 selectField = selectFieldHelper
-    (\theId name attrs inside ->
-      [whamlet| $newline never
-        <div .select>
-          <select ##{theId} name=#{name} *{attrs}>
-            ^{inside}
-      |]) -- outside
-    (\_theId _name isSel ->
-      [whamlet| $newline never
-        <option value=none :isSel:selected>_{MsgSelectNone}
-      |]) -- onOpt
-    (\_theId _name _attrs value isSel text ->
-      [whamlet| $newline never
-        <option value=#{value} :isSel:selected>#{text}
-      |]) -- inside
+  (\theId name attrs inside ->
+    [whamlet| $newline never
+      <div .select>
+        <select ##{theId} name=#{name} *{attrs}>
+          ^{inside}
+    |]) -- outside
+  (\_theId _name isSel ->
+    [whamlet| $newline never
+      <option value=none :isSel:selected>_{MsgSelectNone}
+    |]) -- onOpt
+  (\_theId _name _attrs value isSel text ->
+    [whamlet| $newline never
+      <option value=#{value} :isSel:selected>#{text}
+    |]) -- inside
 
 -- port from Yesod.Form.Fields
 selectFieldHelper
@@ -97,22 +97,22 @@ selectFieldHelper
         -> HandlerT site IO (OptionList a)
         -> Field (HandlerT site IO) a
 selectFieldHelper outside onOpt inside opts' = Field
-    { fieldParse = \x _ -> do
-        opts <- opts'
-        return $ selectParser opts x
-    , fieldView = \theId name attrs val isReq -> do
-        opts <- fmap olOptions $ handlerToWidget opts'
-        outside theId name attrs $ do
-            unless isReq $ onOpt theId name $ not $ render opts val `elem` map optionExternalValue opts
-            flip mapM_ opts $ \opt -> inside
-                theId
-                name
-                ((if isReq then (("required", "required"):) else id) attrs)
-                (optionExternalValue opt)
-                ((render opts val) == optionExternalValue opt)
-                (optionDisplay opt)
-    , fieldEnctype = UrlEncoded
-    }
+  { fieldParse = \x _ -> do
+    opts <- opts'
+    return $ selectParser opts x
+  , fieldView = \theId name attrs val isReq -> do
+    opts <- olOptions <$> handlerToWidget opts'
+    outside theId name attrs $ do
+      unless isReq $ onOpt theId name $ notElem (render opts val) $ map optionExternalValue opts
+      forM_ opts $ \opt -> inside
+        theId
+        name
+        ((if isReq then (("required", "required"):) else id) attrs)
+        (optionExternalValue opt)
+        (render opts val == optionExternalValue opt)
+        (optionDisplay opt)
+  , fieldEnctype = UrlEncoded
+  }
   where
     render _ (Left _) = ""
     render opts (Right a) = maybe "" optionExternalValue $ listToMaybe $ filter ((== a) . optionInternalValue) opts
