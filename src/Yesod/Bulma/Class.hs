a7a1b5e24b32f9805f2d2edff7740a4bcf7c97c0
Shinya Yamaguchi <waddlaw@users.noreply.github.com>
2018-08-05 17:25:36 +0900

    Update Class.hs

--- src/Yesod/Bulma/Class.hs
+++ src/Yesod/Bulma/Class.hs
@@ -11,10 +11,10 @@ import           Yesod.Core
 
 class YesodBulma a where
   urlBulmaCss :: a -> Either (Route a) Text
-  urlBulmaCss _ = Right "//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+  urlBulmaCss _ = Right "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
 
   urlBulmaExCheckRadio :: a -> Either (Route a) Text
-  urlBulmaExCheckRadio _ = Right "//cdn.jsdelivr.net/npm/bulma-extensions@1.0.30/bulma-checkradio/dist/bulma-checkradio.min.css"
+  urlBulmaExCheckRadio _ = Right "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.30/bulma-checkradio/dist/bulma-checkradio.min.css"
 
   urlFontawesomeJs :: a -> Either (Route a) Text
-  urlFontawesomeJs _ = Right "//use.fontawesome.com/releases/v5.2.0/js/all.js"
+  urlFontawesomeJs _ = Right "https://use.fontawesome.com/releases/v5.2.0/js/all.js"
